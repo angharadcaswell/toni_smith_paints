82dac0bdae319d1e706fc5da17161ed42b353aa0
Angharad Caswell <ange.caswell1@gmail.com>
2022-06-02 09:30:28 +0000

    trying to debug linking databases error

--- products/models.py
+++ products/models.py
@@ -30,7 +30,6 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null = True, blank = True, on_delete = models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     sizes = models.ManyToManyField(Size)
 
--- toni_smith_paints/settings.py
+++ toni_smith_paints/settings.py
@@ -99,14 +99,14 @@ WSGI_APPLICATION = 'toni_smith_paints.wsgi.application'
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 else:
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
 
 # Password validation
